b77588780e4078df638f15c5a8586f2924919310
toshiki-haraguchi <tsk.haraguchi@gmail.com>
2022-05-11 00:53:31 +0900

    [tmp]

--- monitoring.sh
+++ monitoring.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-NL=`echo -e "\n"`
+
 ARCH=`uname -a`
 CPU=`grep "physical id" /proc/cpuinfo | wc -l`
 VCPU=`grep "processor" /proc/cpuinfo | wc -l`
 USED=`free -m | grep "Mem" | awk '{print$3}'`
 TOTAL=`free -m | grep "Mem" | awk '{print$2}'`
 UPT=`echo "scale=2; (${USED} / ${TOTAL}) * 100" | bc`
+DISK=`df -h | awk '$NF=="/"{print$3"/"$2" ("$5")"}'`
 
 wall\
        	"\
@@ -13,5 +14,5 @@ wall\
 	#CPU physical : ${CPU}
 	#vCPU : ${VCPU}
 	#Memory Usage : ${USED}/${TOTAL}MB (${UPT}%)
-	#Disk Usage : 
+	#Disk Usage : ${DISK} 
 	"
