2d95f12f40ddc2da7ecb9ec3761ffcf06f0d6f13
toshiki-haraguchi <tsk.haraguchi@gmail.com>
2022-05-11 02:26:47 +0900

    [tmp]

--- monitoring.sh
+++ monitoring.sh
@@ -7,6 +7,9 @@ USED=`free -m | grep "Mem" | awk '{print$3}'`
 TOTAL=`free -m | grep "Mem" | awk '{print$2}'`
 UPT=`echo "scale=2; (${USED} / ${TOTAL}) * 100" | bc`
 DISK=`df -h | awk '$NF=="/"{print$3"/"$2" ("$5")"}'`
+IDLE=`top -bn1 | grep "%Cpu" | tr ',' ' ' | awk '{print$8}'`
+CPU_USE=`echo "scale=1; 100.0 - ${IDLE}" | bc`
+LASTREBOOT=`last reboot | awk 'NR==1{print$5"-"$6"-"$7" "$8}'`
 
 wall\
        	"\
@@ -14,5 +17,7 @@ wall\
 	#CPU physical : ${CPU}
 	#vCPU : ${VCPU}
 	#Memory Usage : ${USED}/${TOTAL}MB (${UPT}%)
-	#Disk Usage : ${DISK} 
+	#Disk Usage : ${DISK}
+	#CPU load : ${CPU_USE}%
+	#Last boot : ${LASTREBOOT} 
 	"
